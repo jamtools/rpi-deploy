0b1cb3f5cc8cf361c6732c2080217e422a575dae
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2025-04-19 12:22:13 -0400

    several updates to clean up unstaged changes

--- updatecli/new_stuff/updatecli.d/scripts/run_from_github_release.sh
+++ updatecli/new_stuff/updatecli.d/scripts/run_from_github_release.sh
@@ -9,3 +9,10 @@ mkdir -p /home/jamtools/code/artifacts/dist
 unzip -o /home/jamtools/code/artifacts/dist.zip -d /home/jamtools/code/artifacts/dist
 
 /home/jamtools/code/scripts/common/create_and_run_service.sh myapp "/usr/bin/node /home/jamtools/code/artifacts/dist/server/dist/local-server.cjs"
+
+source /home/jamtools/code/secrets.env
+
+if [ -n "${WEBHOOK_URL}" ]; then
+  echo "Sending webhook notification"
+  curl -X POST -H "Content-Type: application/json" -d '{"text":"new release deployed"}' "${WEBHOOK_URL}"
+fi

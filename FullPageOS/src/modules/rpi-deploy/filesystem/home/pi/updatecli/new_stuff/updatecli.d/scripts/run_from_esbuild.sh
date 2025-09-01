a7a6f6fbbc80673152184f53c0d62224df65c90b
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2025-09-01 01:20:34 -0400

    delete old folder

--- FullPageOS/src/modules/rpi-deploy/filesystem/home/pi/updatecli/new_stuff/updatecli.d/scripts/run_from_esbuild.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-mkdir -p artifacts
-curl -sL -o /home/pi/code/artifacts/index.js http://jam.local:1380/index.js
-
-/home/pi/code/scripts/common/create_and_run_service.sh $1 "node /home/pi/code/artifacts/index.js"

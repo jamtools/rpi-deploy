f1658edf16c3e9285371225d1e6a430cb52b9df6
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2025-09-02 21:00:34 -0400

    fix substitution issues. add more gh action args for customization

--- FullPageOS/src/modules/rpi-deploy/filesystem/home/pi/code/scripts/run_from_github_release.sh
+++ FullPageOS/src/modules/rpi-deploy/filesystem/home/pi/code/scripts/run_from_github_release.sh
@@ -8,7 +8,7 @@ curl -sL -o /home/pi/code/artifacts/dist.zip "https://github.com/$1/$2/releases/
 mkdir -p /home/pi/code/artifacts/dist
 unzip -o /home/pi/code/artifacts/dist.zip -d /home/pi/code/artifacts/dist
 
-/home/pi/code/scripts/common/create_and_run_service.sh "${SERVICE_NAME:-myapp}" "/usr/bin/node /home/pi/code/artifacts/dist/server/dist/local-server.cjs"
+/home/pi/code/scripts/common/create_and_run_service.sh "${SERVICE_NAME:-myapp}" "${SERVICE_COMMAND}"
 
 source /home/pi/code/secrets.env
 
